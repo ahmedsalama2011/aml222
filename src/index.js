56d4e1e54898e555b71785aa1b036579f5d7cbfd
ahmedsalama2011 <110493433+ahmedsalama2011@users.noreply.github.com>
2022-12-17 08:12:53 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/home';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
